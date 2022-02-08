e639ace007090bbf82177bfe5a1ab62a3d5da45f
Miguel Aguila <maguila.apple@gmail.com>
2022-02-08 15:22:01 +0800

    completed: select user profile picture during registration

--- FoodApp/FoodApp/Controllers/LoginAndRegistration/Registration/RegisterVC.swift
+++ FoodApp/FoodApp/Controllers/LoginAndRegistration/Registration/RegisterVC.swift
@@ -37,6 +37,9 @@ class RegisterVC: UIViewController {
         fillInAddress()
     }
     
+    @IBAction func addPhotoButtonTapped(_ sender: Any) {
+        presentPhotoActionSheet()    }
+    
     @IBAction func locationButtonTapped(_ sender: Any) {
         pushLocationVC()
     }
@@ -179,5 +182,48 @@ extension RegisterVC{
 extension RegisterVC: PassLocationDelegate{
     func passLocation(userInfo: UserInfo) {
         self.userInfo = userInfo
+    }
+}
+
+extension RegisterVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func presentPhotoActionSheet(){
+        let actionSheet = UIAlertController(title: "Profile Picture",
+                                            message: "How would you like to select a picure",
+                                            preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { [weak self] _ in
+            self?.presentCamera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
+        present(actionSheet, animated: true)
+    }
+    
+    func presentCamera(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+        self.personImage.image = selectedImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
